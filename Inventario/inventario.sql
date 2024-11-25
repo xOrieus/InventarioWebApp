e197bc9a908a6e6c16f9e6e7c5e94bfc5ac09668
xOrieus <xOrieus1@gmail.com>
2024-11-25 10:27:38 -0300

    Base de datos actualizada + instrucciones

--- Inventario/inventario.sql
+++ Inventario/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2024 at 06:37 PM
+-- Generation Time: Nov 25, 2024 at 02:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -22,6 +22,533 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `inventario` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `inventario`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group`
+--
+
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group_permissions`
+--
+
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint(20) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_permission`
+--
+
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `auth_permission`
+--
+
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+(1, 'Can add log entry', 1, 'add_logentry'),
+(2, 'Can change log entry', 1, 'change_logentry'),
+(3, 'Can delete log entry', 1, 'delete_logentry'),
+(4, 'Can view log entry', 1, 'view_logentry'),
+(5, 'Can add permission', 2, 'add_permission'),
+(6, 'Can change permission', 2, 'change_permission'),
+(7, 'Can delete permission', 2, 'delete_permission'),
+(8, 'Can view permission', 2, 'view_permission'),
+(9, 'Can add group', 3, 'add_group'),
+(10, 'Can change group', 3, 'change_group'),
+(11, 'Can delete group', 3, 'delete_group'),
+(12, 'Can view group', 3, 'view_group'),
+(13, 'Can add user', 4, 'add_user'),
+(14, 'Can change user', 4, 'change_user'),
+(15, 'Can delete user', 4, 'delete_user'),
+(16, 'Can view user', 4, 'view_user'),
+(17, 'Can add content type', 5, 'add_contenttype'),
+(18, 'Can change content type', 5, 'change_contenttype'),
+(19, 'Can delete content type', 5, 'delete_contenttype'),
+(20, 'Can view content type', 5, 'view_contenttype'),
+(21, 'Can add session', 6, 'add_session'),
+(22, 'Can change session', 6, 'change_session'),
+(23, 'Can delete session', 6, 'delete_session'),
+(24, 'Can view session', 6, 'view_session'),
+(25, 'Can add departamento', 7, 'add_departamento'),
+(26, 'Can change departamento', 7, 'change_departamento'),
+(27, 'Can delete departamento', 7, 'delete_departamento'),
+(28, 'Can view departamento', 7, 'view_departamento'),
+(29, 'Can add proveedor', 8, 'add_proveedor'),
+(30, 'Can change proveedor', 8, 'change_proveedor'),
+(31, 'Can delete proveedor', 8, 'delete_proveedor'),
+(32, 'Can view proveedor', 8, 'view_proveedor'),
+(33, 'Can add producto', 9, 'add_producto'),
+(34, 'Can change producto', 9, 'change_producto'),
+(35, 'Can delete producto', 9, 'delete_producto'),
+(36, 'Can view producto', 9, 'view_producto'),
+(37, 'Can add lote', 10, 'add_lote'),
+(38, 'Can change lote', 10, 'change_lote'),
+(39, 'Can delete lote', 10, 'delete_lote'),
+(40, 'Can view lote', 10, 'view_lote');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user`
+--
+
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_groups`
+--
+
+CREATE TABLE `auth_user_groups` (
+  `id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_user_permissions`
+--
+
+CREATE TABLE `auth_user_user_permissions` (
+  `id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_admin_log`
+--
+
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext DEFAULT NULL,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) UNSIGNED NOT NULL CHECK (`action_flag` >= 0),
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_content_type`
+--
+
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `django_content_type`
+--
+
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+(1, 'admin', 'logentry'),
+(3, 'auth', 'group'),
+(2, 'auth', 'permission'),
+(4, 'auth', 'user'),
+(5, 'contenttypes', 'contenttype'),
+(7, 'InvoPro', 'departamento'),
+(10, 'InvoPro', 'lote'),
+(9, 'InvoPro', 'producto'),
+(8, 'InvoPro', 'proveedor'),
+(6, 'sessions', 'session');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_migrations`
+--
+
+CREATE TABLE `django_migrations` (
+  `id` bigint(20) NOT NULL,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `django_migrations`
+--
+
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+(1, 'InvoPro', '0001_initial', '2024-11-25 13:25:51.031081'),
+(2, 'contenttypes', '0001_initial', '2024-11-25 13:25:51.067166'),
+(3, 'auth', '0001_initial', '2024-11-25 13:25:51.586590'),
+(4, 'admin', '0001_initial', '2024-11-25 13:25:51.712576'),
+(5, 'admin', '0002_logentry_remove_auto_add', '2024-11-25 13:25:51.737832'),
+(6, 'admin', '0003_logentry_add_action_flag_choices', '2024-11-25 13:25:51.745367'),
+(7, 'contenttypes', '0002_remove_content_type_name', '2024-11-25 13:25:51.814774'),
+(8, 'auth', '0002_alter_permission_name_max_length', '2024-11-25 13:25:51.865605'),
+(9, 'auth', '0003_alter_user_email_max_length', '2024-11-25 13:25:51.921581'),
+(10, 'auth', '0004_alter_user_username_opts', '2024-11-25 13:25:51.931595'),
+(11, 'auth', '0005_alter_user_last_login_null', '2024-11-25 13:25:52.070099'),
+(12, 'auth', '0006_require_contenttypes_0002', '2024-11-25 13:25:52.072612'),
+(13, 'auth', '0007_alter_validators_add_error_messages', '2024-11-25 13:25:52.082778'),
+(14, 'auth', '0008_alter_user_username_max_length', '2024-11-25 13:25:52.101739'),
+(15, 'auth', '0009_alter_user_last_name_max_length', '2024-11-25 13:25:52.119314'),
+(16, 'auth', '0010_alter_group_name_max_length', '2024-11-25 13:25:52.143963'),
+(17, 'auth', '0011_update_proxy_permissions', '2024-11-25 13:25:52.164136'),
+(18, 'auth', '0012_alter_user_first_name_max_length', '2024-11-25 13:25:52.184730'),
+(19, 'sessions', '0001_initial', '2024-11-25 13:25:52.237980');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_session`
+--
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invopro_departamento`
+--
+
+CREATE TABLE `invopro_departamento` (
+  `id` bigint(20) NOT NULL,
+  `id_departamento` varchar(16) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invopro_lote`
+--
+
+CREATE TABLE `invopro_lote` (
+  `id` bigint(20) NOT NULL,
+  `id_lote` varchar(16) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `fecha_vencimiento` date NOT NULL,
+  `fecha_fabricacion` date NOT NULL,
+  `cantidad_productos` varchar(10) NOT NULL,
+  `cantidad_disponible` varchar(10) NOT NULL,
+  `producto_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invopro_producto`
+--
+
+CREATE TABLE `invopro_producto` (
+  `id` bigint(20) NOT NULL,
+  `codigo` varchar(16) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `precio_costo` varchar(10) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `precio_venta` varchar(10) NOT NULL,
+  `departamento_id` bigint(20) NOT NULL,
+  `proveedor_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invopro_proveedor`
+--
+
+CREATE TABLE `invopro_proveedor` (
+  `id` bigint(20) NOT NULL,
+  `id_proveedor` varchar(16) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `calle` varchar(30) NOT NULL,
+  `numero` varchar(6) NOT NULL,
+  `ciudad` varchar(30) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `contacto` varchar(50) NOT NULL,
+  `terminos_de_pago` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth_group`
+--
+ALTER TABLE `auth_group`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  ADD KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`);
+
+--
+-- Indexes for table `auth_user`
+--
+ALTER TABLE `auth_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  ADD KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`);
+
+--
+-- Indexes for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  ADD KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  ADD KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`);
+
+--
+-- Indexes for table `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`);
+
+--
+-- Indexes for table `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `django_session`
+--
+ALTER TABLE `django_session`
+  ADD PRIMARY KEY (`session_key`),
+  ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
+
+--
+-- Indexes for table `invopro_departamento`
+--
+ALTER TABLE `invopro_departamento`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_departamento` (`id_departamento`);
+
+--
+-- Indexes for table `invopro_lote`
+--
+ALTER TABLE `invopro_lote`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_lote` (`id_lote`),
+  ADD KEY `InvoPro_lote_producto_id_8a662253_fk_InvoPro_producto_id` (`producto_id`);
+
+--
+-- Indexes for table `invopro_producto`
+--
+ALTER TABLE `invopro_producto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo` (`codigo`),
+  ADD KEY `InvoPro_producto_departamento_id_be13cd2e_fk_InvoPro_d` (`departamento_id`),
+  ADD KEY `InvoPro_producto_proveedor_id_824fe240_fk_InvoPro_proveedor_id` (`proveedor_id`);
+
+--
+-- Indexes for table `invopro_proveedor`
+--
+ALTER TABLE `invopro_proveedor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_proveedor` (`id_proveedor`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `auth_group`
+--
+ALTER TABLE `auth_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `auth_user`
+--
+ALTER TABLE `auth_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `invopro_departamento`
+--
+ALTER TABLE `invopro_departamento`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invopro_lote`
+--
+ALTER TABLE `invopro_lote`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invopro_producto`
+--
+ALTER TABLE `invopro_producto`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invopro_proveedor`
+--
+ALTER TABLE `invopro_proveedor`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`);
+
+--
+-- Constraints for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Constraints for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  ADD CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  ADD CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `invopro_lote`
+--
+ALTER TABLE `invopro_lote`
+  ADD CONSTRAINT `InvoPro_lote_producto_id_8a662253_fk_InvoPro_producto_id` FOREIGN KEY (`producto_id`) REFERENCES `invopro_producto` (`id`);
+
+--
+-- Constraints for table `invopro_producto`
+--
+ALTER TABLE `invopro_producto`
+  ADD CONSTRAINT `InvoPro_producto_departamento_id_be13cd2e_fk_InvoPro_d` FOREIGN KEY (`departamento_id`) REFERENCES `invopro_departamento` (`id`),
+  ADD CONSTRAINT `InvoPro_producto_proveedor_id_824fe240_fk_InvoPro_proveedor_id` FOREIGN KEY (`proveedor_id`) REFERENCES `invopro_proveedor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
